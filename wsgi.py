04afb632abeefa8c1afef0acc7860c9fcec0fad2
Alok Vikram <alokvikram169@gmail.com>
2026-03-29 18:38:03 +0530

    vercel changes

---
+++ wsgi.py
@@ -0,0 +1,7 @@
+"""WSGI entrypoint for Vercel deployment.
+
+This file is created at repo root because Vercel expects one of:
+app.py, index.py, server.py, main.py, wsgi.py, asgi.py
+"""
+
+from Onlinevoting2026.wsgi import application
